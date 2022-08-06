adb24dcbd5f4aca45fc616efbe8f6d75a87599d6
O-SoleMio <dhxodid0207@naver.com>
2022-08-06 23:53:32 +0900

    #project save

---
+++ MainApp.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleWindow
+{
+    class MainApp : System.Windows.Forms.Form
+    {
+        static void Main(string[] args)
+        {
+            System.Windows.Forms.Application.Run(new MainApp()); //  기본 창 생성
+            
+        }
+    }
+}
